7a3301a1f00d9ecfd92321e16c056a2a781e7399
MilkyWay303 <pomidorowy999@gmail.com>
2025-01-16 16:45:08 +0100

    Fixed header and buttons

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-// function showBar1() {
-//     const element = document.getElementById("navBar1");  // Get the DIV element
-//     element.classList.remove("navDis1"); // Remove mystyle class from DIV
-//     element.classList.add("navDis2"); // Add newone class to DIV
-//   }
-
-// function showBar1() {
-//     var element = document.querySelector("#navBar1");
-//     element.classList.replace("navDis1", "navDis2");
-// }
-
 const logo = document.getElementById("hideLogo");
 const button = document.getElementById("showBar1");
 const navbar = document.getElementById("navBar1");

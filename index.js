9948c84dda1671bb52591bfae63f205d9a264d6b
MilkyWay303 <pomidorowy999@gmail.com>
2025-01-21 20:53:45 +0100

    Added footer(unfinished)

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const logo = document.getElementById("hideLogo");
 const button = document.getElementById("showBar1");
 const navbar = document.getElementById("navBar1");
+const body = document.getElementById("body")
 
 button.addEventListener("click", () => {
     navbar.classList.toggle("navDis1");
     navbar.classList.toggle("navDis2");
     logo.classList.toggle("navDis1");
+    body.classList.toggle("block_scroll")
 });
 
